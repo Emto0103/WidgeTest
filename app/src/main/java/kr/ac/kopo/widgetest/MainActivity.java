bf2541bf358c94deeb41e313a44a24022ac4e7f6
emto0103 <emto0103@gmail.com>
2025-03-24 16:26:13 +0900

    4장 기본위젯의 속성들

--- app/src/main/java/kr/ac/kopo/widgetest/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/widgetest/MainActivity.java
@@ -1,6 +1,10 @@
 package kr.ac.kopo.widgetest;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    RadioGroup rg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.enabled_clickable_test);
+
+//        rg = findViewById(R.id.rg);
+//
+//        Button button = findViewById(R.id.rb_button);
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String result = "";
+//
+//                if(rg.getCheckedRadioButtonId() == R.id.rb_male)
+//                    result = getResources().getString(R.string.male);
+//                else
+//                    result = getResources().getString(R.string.female);
+//
+//                Toast toast = Toast.makeText(getApplicationContext(), result+"를 선택하였습니다.", Toast.LENGTH_LONG);
+//                toast.show();
+//
+//            }
+//        });
     }
 }
